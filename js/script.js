fe09b5f096d7be174959c242f08148e6a3b42e35
Gilangnocounter <115324908+Gilangnocounter@users.noreply.github.com>
2026-03-14 01:23:18 +0700

    push

--- js/script.js
+++ js/script.js
@@ -1610,8 +1610,7 @@ function startIntro() {
 			showIntroText(countdown, true);
 			// CUSTOM1
 			setTimeout(() => {
-				// Sau khi số 1 kết thúc animation, hiển thị "HAPPY NEW YEAR"
-				showIntroText('Happy 1st Anniversary, my love.', false);
+				showIntroText('Happy 1st\nAnniversary\nAnita my love\n💗', false);
 				
 				// Sau khi "HAPPY NEW YEAR" hiển thị 1 giây (0.8s animation + 0.2s), bắt đầu pháo hoa ngay
 				setTimeout(() => {
